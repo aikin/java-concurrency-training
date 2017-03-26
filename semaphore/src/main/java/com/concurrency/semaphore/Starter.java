18bdf7b4cd248add89dda13dc6efa36c43d38a48
aikin <1@aikin.me>
2017-03-26 23:07:54 +0800

    aikin: add semaphore

---
+++ semaphore/src/main/java/com/concurrency/semaphore/Starter.java
@@ -0,0 +1,8 @@
+package com.concurrency.semaphore;
+
+public class Starter {
+    public static void main(String[] args) {
+        BoundHashSet<Integer> boundHashSet = new BoundHashSet<Integer>(5);
+        
+    }
+}
