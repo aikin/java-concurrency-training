e9cc21024b08cc9413d7ff868ce0f3c7f96da47f
aikin <1@aikin.me>
2017-03-30 00:28:55 +0800

    aikin: use ConcurrentHaspMap improve concurrency profermance

--- cache/src/main/java/com/concurrency/cache/Memorizer.java
+++ cache/src/main/java/com/concurrency/cache/Memorizer.java
@@ -1,19 +1,18 @@
 package com.concurrency.cache;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 class Memorizer {
     private final Computable computable;
-    private final Map<Double, Double> cache = new HashMap<>();
+    private final Map<Double, Double> cache = new ConcurrentHashMap<>();
 
     Memorizer(Computable computable) {
         this.computable = computable;
     }
 
-    // TODO: can not deal with concurrency
-    synchronized double compute(double number) {
+    double compute(double number) {
         Double result = cache.get(number);
 
         if (Objects.isNull(result)) {
