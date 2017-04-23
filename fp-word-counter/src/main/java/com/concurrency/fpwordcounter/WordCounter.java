4f1edbd3ceb16750ab5674f252dbb5911ed85a14
aikin <1@aikin.me>
2017-04-23 23:02:41 +0800

    aikin: replace switch

--- fp-word-counter/src/main/java/com/concurrency/fpwordcounter/WordCounter.java
+++ fp-word-counter/src/main/java/com/concurrency/fpwordcounter/WordCounter.java
@@ -7,13 +7,16 @@ import javax.xml.stream.XMLStreamReader;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class WordCounter {
+
+    private final static Logger logger = Logger.getLogger("fp.word.counter");
 
     public static void main(String[] args) throws XMLStreamException {
 
         InputStream resourceAsStream = WordCounter.class.getClassLoader().getResourceAsStream("enwiki.xml");
-
         XMLStreamReader xmlStreamReader = XMLInputFactory.newInstance().createXMLStreamReader(resourceAsStream);
 
         Page page = null;
@@ -22,35 +25,27 @@ public class WordCounter {
 
         while (xmlStreamReader.hasNext()) {
             int event = xmlStreamReader.next();
-            
-            switch (event) {
-                case XMLStreamConstants.START_ELEMENT:
-                    if ("page".equals(xmlStreamReader.getLocalName())) {
-                        page = new Page();
-                    }
-                    break;
 
-                case XMLStreamConstants.CHARACTERS:
-                    tagContent = xmlStreamReader.getText().trim();
-                    break;
-
-                case XMLStreamConstants.END_ELEMENT:
-                    switch (xmlStreamReader.getLocalName()) {
-                        case "page":
-                            pages.add(page);
-                            break;
-                        case "title":
-                            page.setTitle(tagContent);
-                            break;
-                        case "text":
-                            page.setText(tagContent);
-                            break;
-                    }
-                    break;
+            if (event == XMLStreamConstants.START_ELEMENT && "page".equals(xmlStreamReader.getLocalName())) {
+                page = new Page();
+            }
+            if (event == XMLStreamConstants.CHARACTERS) {
+                tagContent = xmlStreamReader.getText().trim();
+            }
+            if (event == XMLStreamConstants.END_ELEMENT) {
+                if ("page".equals(xmlStreamReader.getLocalName())) {
+                    pages.add(page);
+                }
+                if ("title".equals(xmlStreamReader.getLocalName())) {
+                    page.setTitle(tagContent);
+                }
+                if ("text".equals(xmlStreamReader.getLocalName())) {
+                    page.setText(tagContent);
+                }
             }
         }
 
-        System.out.printf("Pages size: " + pages.size());
+        logger.log(Level.INFO,"Pages size: {0}", pages.size());
 
     }
 }
