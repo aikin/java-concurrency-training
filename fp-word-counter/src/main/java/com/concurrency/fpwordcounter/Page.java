f4f9473b16c535a7beb7474ad97b45ba3de721c4
aikin <1@aikin.me>
2017-04-23 23:01:08 +0800

    aikin: parse page

---
+++ fp-word-counter/src/main/java/com/concurrency/fpwordcounter/Page.java
@@ -0,0 +1,25 @@
+package com.concurrency.fpwordcounter;
+
+class Page {
+    Page() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    private String title;
+    private String text;
+}
--- fp-word-counter/src/main/java/com/concurrency/fpwordcounter/WordCounter.java
+++ fp-word-counter/src/main/java/com/concurrency/fpwordcounter/WordCounter.java
@@ -1,7 +1,56 @@
 package com.concurrency.fpwordcounter;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordCounter {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws XMLStreamException {
+
+        InputStream resourceAsStream = WordCounter.class.getClassLoader().getResourceAsStream("enwiki.xml");
+
+        XMLStreamReader xmlStreamReader = XMLInputFactory.newInstance().createXMLStreamReader(resourceAsStream);
+
+        Page page = null;
+        String tagContent = null;
+        List<Page> pages = new ArrayList<>();
+
+        while (xmlStreamReader.hasNext()) {
+            int event = xmlStreamReader.next();
+            
+            switch (event) {
+                case XMLStreamConstants.START_ELEMENT:
+                    if ("page".equals(xmlStreamReader.getLocalName())) {
+                        page = new Page();
+                    }
+                    break;
+
+                case XMLStreamConstants.CHARACTERS:
+                    tagContent = xmlStreamReader.getText().trim();
+                    break;
+
+                case XMLStreamConstants.END_ELEMENT:
+                    switch (xmlStreamReader.getLocalName()) {
+                        case "page":
+                            pages.add(page);
+                            break;
+                        case "title":
+                            page.setTitle(tagContent);
+                            break;
+                        case "text":
+                            page.setText(tagContent);
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        System.out.printf("Pages size: " + pages.size());
+
     }
 }
